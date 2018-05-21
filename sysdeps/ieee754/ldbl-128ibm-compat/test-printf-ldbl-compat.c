ca3383ddabd3c6bc09928fea69e5cd43642d4190
Gabriel F. T. Gomes <gabriel@inconstante.eti.br>
2018-05-21 11:25:39 -0300

    ldbl-128ibm-compat: Add regular character printing functions
    The 'mode' argument to __vfprintf_internal allows the selection of the
    long double format for all long double arguments requested by the format
    string.  Currently, there are two possibilities: long double with the
    same format as double or long double as something else.  The 'something
    else' format varies between architectures, and on powerpc64le, it means
    IBM Extended Precision format.
    
    In preparation for the third option of long double format on
    powerpc64le, this patch uses the new mode mask,
    PRINTF_LDBL_USES_FLOAT128, which tells __vfprintf_internal to save the
    floating-point values into variables of type __float128 and adjusts the
    parameters to __printf_fp and __printf_fphex as if it was a call from
    strfromf128.
    
    Tested for powerpc64le.
    
    	* elf/tst-addr1.c (do_test): Accept redirections of printf to
    	__printfieee128.
    
    	* sysdeps/ieee754/ldbl-128ibm-compat/Makefile:
    	[subdir == stdio-common] (ldbl-extra-routines): Add asprintf,
    	dprintf, fprintf, printf, snprintf, sprintf, vasprintf,
    	vdprintf, vfprintf, vprintf, vsnprintf, and vsprintf.
    	[subdir == stdio-common] (CFLAGS-vfprintf-internal.c): New
    	variable.  Add -mfloat128 to the compilation of
    	vfprintf-internal.c, so that it gets support for the use of
    	__printf_fp and __printf_fphex with __float128 parameter.
    	[subdir == stdio-common] (tests-internal): Add
    	test-printf-ieee128 and test-printf-ibm128.
    	[subdir == stdio-common] (CFLAGS-test-printf-ieee128.c): New
    	variable to add the relevant -mabi flags to the compilation.
    	[subdir == stdio-common] (CFLAGS-test-printf-ibm128.c): Likewise.
    	[subdir == stdio-common && run-built-tests == yes]
    	(tests-special): Add $(objpfx)test-printf-ieee128.out and
    	$(objpfx)test-printf-ibm128.out.
    	[subdir == stdio-common] ($(objpfx)test-printf-ieee128.out):
    	New build and test rule.
    	[subdir == stdio-common] ($(objpfx)test-printf-ibm128.out):
    	Likewise.
    
    	* sysdeps/ieee754/ldbl-128ibm-compat/Versions: (libc): Add
    	__asprintfieee128, __dprintfieee128, __fprintfieee128,
    	__printfieee128, __snprintfieee128, __sprintfieee128,
    	__vasprintfieee128, __vdprintfieee128, __vfprintfieee128,
    	__vprintfieee128, __vsnprintfieee128, and __vsprintfieee128.
    
    	* sysdeps/ieee754/ldbl-128ibm-compat/ieee128-asprintf.c: New file.
    	* sysdeps/ieee754/ldbl-128ibm-compat/ieee128-dprintf.c: Likewise.
    	* sysdeps/ieee754/ldbl-128ibm-compat/ieee128-fprintf.c: Likewise.
    	* sysdeps/ieee754/ldbl-128ibm-compat/ieee128-printf.c: Likewise.
    	* sysdeps/ieee754/ldbl-128ibm-compat/ieee128-snprintf.c: Likewise.
    	* sysdeps/ieee754/ldbl-128ibm-compat/ieee128-sprintf.c: Likewise.
    	* sysdeps/ieee754/ldbl-128ibm-compat/ieee128-vasprintf.c: Likewise.
    	* sysdeps/ieee754/ldbl-128ibm-compat/ieee128-vdprintf.c: Likewise.
    	* sysdeps/ieee754/ldbl-128ibm-compat/ieee128-vfprintf.c: Likewise.
    	* sysdeps/ieee754/ldbl-128ibm-compat/ieee128-vprintf.c: Likewise.
    	* sysdeps/ieee754/ldbl-128ibm-compat/ieee128-vsnprintf.c: Likewise.
    	* sysdeps/ieee754/ldbl-128ibm-compat/ieee128-vsprintf.c: Likewise.
    	* sysdeps/ieee754/ldbl-128ibm-compat/test-printf-ibm128.c: Likewise.
    	* sysdeps/ieee754/ldbl-128ibm-compat/test-printf-ieee128.c: Likewise.
    	* sysdeps/ieee754/ldbl-128ibm-compat/test-printf-ldbl-compat.c:
    	Likewise.
    	* sysdeps/ieee754/ldbl-128ibm-compat/test-printf-ldbl-compat.sh:
    	Likewise.
    	* sysdeps/unix/sysv/linux/powerpc/powerpc64/le/ldbl-128ibm-compat-abi.h:
    	Likewise.

---
+++ sysdeps/ieee754/ldbl-128ibm-compat/test-printf-ldbl-compat.c
@@ -0,0 +1,134 @@
+/* Test for the long double variants of *printf functions.
+   Copyright (C) 2018 Free Software Foundation, Inc.
+   This file is part of the GNU C Library.
+
+   The GNU C Library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Lesser General Public
+   License as published by the Free Software Foundation; either
+   version 2.1 of the License, or (at your option) any later version.
+
+   The GNU C Library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Lesser General Public License for more details.
+
+   You should have received a copy of the GNU Lesser General Public
+   License along with the GNU C Library; if not, see
+   <http://www.gnu.org/licenses/>.  */
+
+#include <stdarg.h>
+#include <stdint.h>
+#include <stdio.h>
+
+#include <support/check.h>
+
+static void
+do_test_call_varg (FILE *stream, const char *format, ...)
+{
+  char *buffer = NULL;
+  char string[128];
+  va_list args;
+
+  printf ("%20s", "vasprintf: ");
+  va_start (args, format);
+  vasprintf (&buffer, format, args);
+  va_end (args);
+  if (buffer == NULL)
+    printf ("Error using vasprintf\n");
+  else
+    {
+      printf ("%s", buffer);
+      free (buffer);
+    }
+  printf ("\n");
+
+  printf ("%20s", "vdprintf: ");
+  va_start (args, format);
+  vdprintf (1, format, args);
+  va_end (args);
+  printf ("\n");
+
+  printf ("%20s", "vfprintf: ");
+  va_start (args, format);
+  vfprintf (stream, format, args);
+  va_end (args);
+  printf ("\n");
+
+  printf ("%20s", "vprintf: ");
+  va_start (args, format);
+  vprintf (format, args);
+  va_end (args);
+  printf ("\n");
+
+  printf ("%20s", "vsnprintf: ");
+  va_start (args, format);
+  vsnprintf (string, 127, format, args);
+  va_end (args);
+  printf ("%s", string);
+  printf ("\n");
+
+  printf ("%20s", "vsprintf: ");
+  va_start (args, format);
+  vsprintf (string, format, args);
+  va_end (args);
+  printf ("%s", string);
+  printf ("\n");
+}
+
+static void
+do_test_call_rarg (FILE *stream, const char *format, long double ld)
+{
+  char *buffer = NULL;
+  char string[128];
+
+  printf ("%20s", "asprintf: ");
+  asprintf (&buffer, format, ld);
+  if (buffer == NULL)
+    printf ("Error using asprintf\n");
+  else
+    {
+      printf ("%s", buffer);
+      free (buffer);
+    }
+  printf ("\n");
+
+  printf ("%20s", "dprintf: ");
+  dprintf (1, format, ld);
+  printf ("\n");
+
+  printf ("%20s", "fprintf: ");
+  fprintf (stdout, format, ld);
+  printf ("\n");
+
+  printf ("%20s", "printf: ");
+  printf (format, ld);
+  printf ("\n");
+
+  printf ("%20s", "snprintf: ");
+  snprintf (string, 127, format, ld);
+  printf ("%s", string);
+  printf ("\n");
+
+  printf ("%20s", "sprintf: ");
+  sprintf (string, format, ld);
+  printf ("%s", string);
+  printf ("\n");
+}
+
+static int
+do_test (void)
+{
+  long double ld = -1;
+
+  /* Print in decimal notation.  */
+  do_test_call_rarg (stdout, "%.60Lf", ld);
+  do_test_call_varg (stdout, "%.60Lf", ld);
+
+  /* Print in hexadecimal notation.  */
+  do_test_call_rarg (stdout, "%.60La", ld);
+  do_test_call_varg (stdout, "%.60La", ld);
+
+  return 0;
+}
+
+#include <support/test-driver.c>
